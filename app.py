5adbd732d4b18c2b9ead49d0827be3846c263daa
JarDuitama <jard_loco@hotmail.col>
2023-09-05 15:34:28 -0500

    Ajuste de clave

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from ArchAux import CreaBD as crear
 from ArchAux import ReconoFace as registro
-import os, getpass
+import os, getpass, shutil
 
 def app():
     os.system("cls")
@@ -9,35 +9,38 @@ def app():
         
         1. Crear nuevo Perfil
         2. Registrar Entrad/Salida
-        3. Traer Archivo Registro
+        3. Exportar Archivo Registro
         4. Salir
         **
-            Opcion: """)
+            Opcion: """
+    )
 
-    if opc != '1' and opc!='2' and opc!='3':
-        print('Opcion no Valida')
-        app()
-    elif opc == '1':
-        p = getpass.getpass(prompt='\t\tIngese clave: ')
-        if p == '1073672345':
-            crear.main()
-        else:
-            print("Calve NO es correcta")
+    match opc:
+        case '1':
+            p = getpass.getpass(prompt='\t\tIngese clave: ')
+            if p == '12345':
+                crear.main()
+            else:
+                print("Calve NO es correcta")
+                app()
+        case '2':
+           registro.main()
+        case '3':
+            os.system("cls")
+            print("\n")
+            print("Copiando Archivo de Registros\n\n")
+            #rutaData = os.getcwd()
+            archivo = 'Registro.csv'
+            if (os.path.exists(archivo)):
+                rutaDestino = input("Ingrese ruta de descarga:")
+                shutil.copy(archivo,rutaDestino)
+
+        case '4':
+            exit
+
+        case _:
+            print('Opcion no Valida')
             app()
-    elif opc == '2':
-        registro.main()
-    elif opc == '3':
-        os.system("cls")
-        print("Copiando Archivo de Registros")
-        #rutaData = os.getcwd()
-        archivo = 'Registro.csv'        
-        print(os.path.exists(archivo))
-        
-        #shutil.copy(archivo,"Downloads\\"+archivo)
-
-
-    else:
-        exit()
 
 if __name__ == "__main__":
     app()
